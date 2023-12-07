74e6bd10ab36c2bc63fe91fda75bc4f5d53c2fd6
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2023-12-07 19:15:26 -0300

    Adicionar arquivos de projeto.

---
+++ Core/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace Core
+{
+    public class Usuario
+    {
+        public Usuario(int id, string nome, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Email { get; set;}
+    }
+}
